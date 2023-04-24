48a085c5462aa02471b598f4a6a966e5b1bf6375
Harshith D R <harshithdr10@gmail.com>
2023-04-25 01:03:12 +0530

    update

---
+++ bulk photo date changer.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import piexif
+
+
+
+if __name__ == '__main__':
+    for x in range(93,100):
+        filename = 'C:\\Users\\91911\\Desktop\\100D5300\\1\\23\\DSC_00'+str(x)+'.JPG'
+        exif_dict = piexif.load(filename)
+        new_date = datetime(2021, 11, 6, 00, 00, 00).strftime("%Y:%m:%d %H:%M:%S")
+        exif_dict['0th'][piexif.ImageIFD.DateTime] = new_date
+        exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = new_date
+        exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = new_date
+        exif_bytes = piexif.dump(exif_dict)
+        piexif.insert(exif_bytes, filename)
+        print(filename)
